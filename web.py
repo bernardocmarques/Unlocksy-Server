b486b17d5ab0bfda7b73a4d7661a0baebd918f2b
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2019-12-06 13:56:37 +0000

    some front-end done

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 import threading
 
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, url_for
 from server import BT_Server
 from flask_qrcode import QRcode
 
@@ -15,17 +15,12 @@ server = BT_Server(rsa)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html", device_name='') #FIXME pass device-name
 
 
-@app.route("/run")
-def run():
-    print(server.isRunning)
-    if server.isRunning:
-        return "Server already running"
-    else:
-        threading.Thread(target=server.run_server).start()
-        return "server running"
+@app.route("/manage-devices")
+def manageDevices():
+    return render_template("manage-devices.html", device_name='') #FIXME pass device-name
 
 
 def get_mac():
@@ -37,12 +32,26 @@ def get_mac():
     bt_mac = output.split("{}:".format(device_id))[1].split("BD Address: ")[1].split(" ")[0].strip()
     return bt_mac
 
-
-@app.route("/qrcode", methods=["GET"])
-def get_qrcode():
+@app.route("/qr-code", methods=["GET"])
+def qrCode():
     data = get_mac() + "\n"
     data += rsa.get_public_key_base64()
-    return render_template("qrcode.html", qrcode_img=qrcode(data))
+    return render_template("qr-code.html", qrcode_img=qrcode(data), device_name='') #FIXME pass device-name
+
+
+@app.route("/manage-files")
+def manageFiles():
+    return render_template("manage-files.html", device_name='') #FIXME pass device-name
+
+
+@app.route("/run")
+def run():
+    print(server.isRunning)
+    if server.isRunning:
+        return "Server already running"
+    else:
+        threading.Thread(target=server.run_server).start()
+        return "server running"
 
 
 if __name__ == "__main__":
